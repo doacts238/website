25ef6610950244a285d9dfe902e6c4337759044a
Mike R. Prevost <mprevost@gradkell.com>
2020-01-08 12:12:57 -0600

    Removed from App all the old stuff from when it handled mobile display

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,9 @@ import {
   Typography,
   AppBar,
   Toolbar,
-  IconButton,
-  Hidden,
   Container,
   withStyles
 } from '@material-ui/core';
-
-import MenuIcon from 'mdi-react/MenuIcon';
-import CloseIcon from 'mdi-react/CloseIcon';
 
 import classNames from 'classnames';
 import GridNoPadding from './GridNoPadding';
@@ -30,78 +25,28 @@ type Props = {
   location: Object,
   history: Object
 };
-type State = {
-  isLargeScreen: Boolean,
-  isNavListOpen: boolean,
-  isMounted: true,
-  appBarHeight: Number
-};
-
-const DETECTOR_SMALL = 1;
-const DETECTOR_LARGE = 2;
+type State = {};
 
 class App extends Component<Props, State> {
-  state: State = {
-    isLargeScreen: false,
-    isNavListOpen: false,
-    isMounted: false,
-    appBarHeight: 0
-  };
+  state: State = {};
 
   constructor(props) {
     super(props);
-    this.detectorRef = React.createRef();
     this.contentRef = React.createRef();
   }
 
-  componentDidMount() {
-    const detectorWidth: Number = this.getDetectorWidth();
-    const isLargeScreen = detectorWidth === DETECTOR_LARGE;
-    this.setState({
-      isLargeScreen: isLargeScreen,
-      isNavListOpen: isLargeScreen,
-      isMounted: true
-    });
-  }
-
-  getDetectorWidth = (): Number => {
-    let retVal: Number = -1;
-
-    const detectorElement = this.detectorRef.current;
-    if (detectorElement && detectorElement.getBoundingClientRect) {
-      const detectorWidth = detectorElement.getBoundingClientRect().width;
-      if (typeof detectorWidth === 'number') {
-        retVal = detectorWidth;
-      }
-    }
-
-    return retVal;
-  };
-
-  handleNavMenuButtonClick = () => {
-    const { isNavListOpen } = this.state;
-    this.setState({
-      isNavListOpen: !isNavListOpen
-    });
-  };
-
   handleListItemClick = strUrl => {
     const { history } = this.props;
-    const { isNavListOpen, isLargeScreen } = this.state;
 
     if (history) {
       history.push({
         pathname: strUrl
       });
-      if (!isLargeScreen && isNavListOpen) {
-        this.setState({ isNavListOpen: false });
-      }
     }
   };
 
   render() {
     const { className, classes } = this.props;
-    const { isNavListOpen, isLargeScreen } = this.state;
 
     // Overcome scroll position issue caused  by React Router.
     // See https://reacttraining.com/react-router/web/guides/scroll-restoration
@@ -111,34 +56,15 @@ class App extends Component<Props, State> {
       this.contentRef.current.scrollTop = 0;
     }
 
-    let menuIcon = <MenuIcon className={classes.menuButton} />;
-    if (isNavListOpen) {
-      menuIcon = <CloseIcon className={classes.menuButton} />;
-    }
-
     return (
       <div id="appRoot" className={classNames(className, classes.root)}>
-        {/**/}
         <AppBar className={classes.appBar} position="static">
           <Toolbar>
-            <Hidden mdUp>
-              <IconButton
-                className={classes.menuButton}
-                onClick={this.handleNavMenuButtonClick}
-                color="inherit"
-                aria-label="open menu"
-                edge="start"
-              >
-                {menuIcon}
-              </IconButton>
-            </Hidden>
             <Typography variant="h6" noWrap>
               Do Acts 2:38!
             </Typography>
           </Toolbar>
         </AppBar>
-        {/**/}
-        <div ref={this.detectorRef} className={classes.detector} />
         <GridNoPadding id="app_grid_container" container spacing={0}>
           <GridNoPadding item>
             <NavList
@@ -152,12 +78,6 @@ class App extends Component<Props, State> {
               className={classNames(classes.content)}
               ref={this.contentRef}
             >
-              <div
-                className={classNames({
-                  [classes.myBackdrop]: true,
-                  [classes.displayNone]: isLargeScreen || !isNavListOpen
-                })}
-              />
               <Switch>
                 <Route
                   path="/doacts238"
@@ -187,7 +107,6 @@ class App extends Component<Props, State> {
                   render={props => <PageHome {...props} />}
                 />
               </Switch>
-              {/*<Toolbar />*/}
             </Container>
           </GridNoPadding>
         </GridNoPadding>
@@ -210,9 +129,6 @@ const styles = theme => ({
   appBar: {
     height: theme.spacing(theme.app.page.appBarHeight)
   },
-  button: {
-    margin: theme.spacing(1)
-  },
   navListItem: {
     padding: theme.spacing(2),
     paddingRight: theme.spacing(1),
@@ -222,51 +138,14 @@ const styles = theme => ({
   content: {
     //border: '2px solid blue',
     position: 'fixed',
-    [theme.breakpoints.down('sm')]: {
-      maxWidth: theme.spacing(theme.app.page.pageMaxWidth)
-    },
-    [theme.breakpoints.up('md')]: {
-      maxWidth: theme.spacing(
-        theme.app.page.pageMaxWidth - theme.app.page.navListWidth
-      )
-    },
+    maxWidth: theme.spacing(
+      theme.app.page.pageMaxWidth - theme.app.page.navListWidth
+    ),
     // The following isn't TOO weird because the MUI examples also use it.
     height: `calc(100% - ${theme.spacing(theme.app.page.appBarHeight)}px)`,
     overflow: 'auto',
     margin: 0,
     padding: 0
-  },
-  detector: {
-    visibility: 'hidden',
-    height: 0,
-    [theme.breakpoints.down('sm')]: {
-      width: `${DETECTOR_SMALL}px`
-    },
-    [theme.breakpoints.up('md')]: {
-      width: `${DETECTOR_LARGE}px`
-    }
-  },
-  backdrop: {
-    zIndex: theme.zIndex.drawer + 1
-  },
-  menuButton: {},
-  myBackdrop: {
-    zIndex: theme.zIndex.drawer + 1,
-    position: 'absolute',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    right: 0,
-    bottom: 0,
-    top: 0,
-    left: 0,
-    backgroundColor: 'rgba(0, 0, 0, 0.10)',
-    WebkitTapHighlightColor: 'transparent',
-    // Disable scroll capabilities.
-    touchAction: 'none'
-  },
-  displayNone: {
-    display: 'none'
   }
   //offset: theme.mixins.toolbar
 });
